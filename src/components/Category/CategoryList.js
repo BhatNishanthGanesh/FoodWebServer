d1f2b20b5c17150e887cd9773eabe9699fb2330d
tejas-gk <tejgankri@outlook.com>
2023-04-03 21:46:09 +0530

    feat:add to cart

--- src/components/Category/CategoryList.js
+++ src/components/Category/CategoryList.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React,{useState} from 'react'
 import "./Category.scss"
 import {Link} from "react-router-dom"
 const CategoryList = ({categories})=> {
   console.log(categories);
+  const [count, setCount] = useState(0);
+
+
   return (
     <div className='section-wrapper bg-whitesmoke'>
       <div className='container'>
@@ -12,16 +15,39 @@ const CategoryList = ({categories})=> {
           categories.map(category=>{
             const {idCategory: id,strCategory: title, strCategoryThumb:thumbnail}=category;
 
-            return(
-              <Link to = {`/meal/category/${title}`}className="category-itm align-center justify-center" key={id}>
+            return (
+              <>
+                <div className = 'category-itm align-center justify-center'
+                key = {
+                  id
+                } >
+              <Link to = {`/meal/category/${title}`} >
                 <div className='category-itm-img h-100 w-100 flex align-center justify-center'>
                  <img src={thumbnail} alt={title}/>
                  <div className='category-itm-title bg-orange'>
                   <h3 className='text-white fs-11 fw-6 ls-1 text-uppercase'>{title}</h3>
                  </div>
               </div>
-              </Link>
+                  </Link>
+                  <div className='align-center justify-end ml-auto
+                   flex-row ml-auto px-4 pb-7 category-itm-btns__justify-center
+                  '>
 
+                    <button
+                      onClick={() => setCount(count + 1)}
+                      className={`category-itm-btn plus-btn`}>+</button>
+                    <p className='ml-auto count'>
+                      {count}
+                    </p>
+                    <button
+                      onClick={() => {
+                        if(count === 0) return;
+                        setCount(count - 1)
+                      }}
+                      className={`category-itm-btn`}>-</button>
+                  </div>
+                </div>
+              </>
             )
           })
         }
